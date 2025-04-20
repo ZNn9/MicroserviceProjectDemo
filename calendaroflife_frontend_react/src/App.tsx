5cec41a9eb690db8457d568355ebc43625c9886b
Nguyen Dai Hiep <nightz2622@gmail.com>
2025-04-20 19:36:41 +0700

    Docker composer 75%, chưa test API Gateway và tích hợp API lên FE

---
+++ calendaroflife_frontend_react/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import LoginPage from './pages/Account/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
